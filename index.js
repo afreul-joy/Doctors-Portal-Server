36f919f8e65c54192d3f2c79efbce6091b0a1fef
Afreul kaium Joy <afreuljoy@gmail.com>
2021-11-06 20:31:52 +0600

    server added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vljpp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -14,8 +15,15 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
   try {
     await client.connect();
-    console.log("Connected to dataBase Succesfully");
-    
+    const database = client.db('doctors_portal')
+    const appointmentsCollection = database.collection('appointments');
+
+    app.post('/appointments', async (req, res) => {
+      const appointment = req.body
+      const result = await appointmentsCollection.insertOne(appointment)
+      console.log(result);
+      res.json(result)
+    })
   } finally {
     // await client.close(); 
   }
@@ -23,9 +31,11 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello Doctors Portal!')
+  res.send('Hello Doctorss Portal!')
 })
 
 app.listen(port, () => {
   console.log(`listening at ${port}`)
 })
+
+
